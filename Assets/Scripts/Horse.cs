56802c7cc00b25986fd3714d270e935b3020dfbf
pixelshade <psuldo@gmail.com>
2016-09-30 21:11:31 +0200

    horseprefab, horseClass

---
+++ Assets/Scripts/Horse.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Horse
+{
+    public string Name = "NoName";
+    public string Sprite = "NoSprite";
+    public float Speed = 1f;
+    public float Luck = 0f;
+    public float Condition = 1f;
+    public float Bonus = 1f;
+
+    public Horse()
+    {
+
+    }
+
+    public Horse(string name, string sprite, float speed, float luck, float condition)
+    {
+        Name = name;
+        Sprite = sprite;
+        Speed = speed;
+        Luck = luck;
+        Condition = condition;
+    }
+
+
+    public float GetPos()
+    {
+        float bonus = 0;
+        if(Random.Range(0,1) > Luck)
+        {
+            bonus = Bonus;
+        }
+        return Speed * Condition + bonus;
+    }
+
+    
+    public override string ToString()
+    {
+        return Name+" sprite:'"+ "' speed:'"+ Speed + "' Luck:'"+ Luck+"' Condition:'"+Condition+"'";
+    }
+}
---
+++ Assets/Scripts/HorsePrefabScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HorsePrefabScript : MonoBehaviour {
+    [SerializeField]
+    private Text horseNameText;
+
+    [SerializeField]
+    private SpriteRenderer horseSprite;
+
+    private Animator animator;
+    private Horse horse;
+
+
+    private bool moving;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (moving)
+        {
+            horse.GetPos();
+        }
+    }
+
+    public void SetHorse(Horse horse)
+    {
+        this.horse = horse;
+        horseNameText.text = horse.Name;
+        horseSprite.sprite = Resources.Load(horse.Sprite) as Sprite;
+    }
+
+    public void StartRunning()
+    {
+        moving = true;
+        animator.SetTrigger("running");
+    }
+
+    public void StopRunning()
+    {
+        moving = false;
+        animator.SetTrigger("normal");
+    }
+}
